cd76c80cbd87f0e43f7408926ef81d07f943b417
Laura <laurallamasrod@gmail.com>
2026-04-16 18:19:58 +0200

    Arreglos

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,7 +1,7 @@
 import type { Coffee } from "../types/coffee.js";
 
 export async function getCoffees(): Promise<Coffee[]> {
-  const response = await fetch("https://the-black-cat-brew.onrender.com");
+  const response = await fetch("https://the-black-cat-brew.onrender.com/api/v1/coffees");
 
   if (!response.ok) {
     throw new Error("Error fetching coffees");
