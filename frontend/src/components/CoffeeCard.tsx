a2d195f464f1ed88fc648e6a9b1be4435b05013d
Laura <laurallamasrod@gmail.com>
2026-04-17 14:10:21 +0200

    Error arreglado

--- frontend/src/components/CoffeeCard.tsx
+++ frontend/src/components/CoffeeCard.tsx
@@ -21,7 +21,7 @@ function CoffeeCard({ coffee, favView = false }: Props) {
   return (
     <article className="overflow-hidden rounded-2xl bg-secondary p-3 shadow-md shadow-black/10 transition hover:-translate-y-1 hover:shadow-xl">
       {/* Imagen */}
-      <div className="overflow-hidden rounded-full bg-primary">
+      <div className="overflow-hidden rounded-xl bg-primary">
         <img
           src={coffee.image}
           alt={coffee.name}
