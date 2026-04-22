9d17af149934d70adf1a79782f94234b2f1c8cc1
Laura <laurallamasrod@gmail.com>
2026-04-22 17:40:17 +0200

    comentado error

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -5,7 +5,7 @@ export async function getCoffees(): Promise<Coffee[]> {
   const local = "http://localhost:3000/api/v1/coffees";
 
   //fetch
-  const deployed = "https://the-black-cat-brew.onrender.com/api/v1/coffees";
+ // const deployed = "https://the-black-cat-brew.onrender.com/api/v1/coffees";
   const response = await fetch(local);
 
   if (!response.ok) {
