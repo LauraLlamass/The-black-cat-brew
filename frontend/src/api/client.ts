377dbff6a6d9f5e2fdfe7dc34be1ffe0e0eb9c66
Laura <laurallamasrod@gmail.com>
2026-04-24 12:27:29 +0200

    deployed backend listo

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -2,11 +2,13 @@ import type { Coffee } from "../types/coffee.js";
 
 export async function getCoffees(): Promise<Coffee[]> {
   //url por entorno
-  const local = "http://localhost:3000/api/v1/coffees";
+//  const local = "http://localhost:3000/api/v1/coffees";
 
   //fetch
- // const deployed = "https://the-black-cat-brew.onrender.com/api/v1/coffees";
-  const response = await fetch(local);
+  const deployed = "https://the-black-cat-brew.onrender.com/api/v1/coffees";
+
+  //cambiar "local" y "deployed" segun forma de trabajo
+  const response = await fetch(deployed);
 
   if (!response.ok) {
     throw new Error("Error fetching coffees");
